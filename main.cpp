f03a3e5d8e051176cd31c608fe03380eb67eb32d
Truc D T Nguyen <truc.ndtt@gmail.com>
2019-11-21 13:03:28 -0500

    Some comments in main

--- main.cpp
+++ main.cpp
@@ -2,17 +2,8 @@
 
 int main(int argc, char **argv)
 {
-    Simulator::initialize(argv[1]);
-    Simulator::loop();
+    Simulator::initialize(argv[1]); // initialize the Simulator
+    Simulator::loop(); // start the Simulator
 
-//    RBT* rbt = new RBT();
-//    rbt->unitTest();
-
-//    MinHeap::unitTest();
-
-//    NodeRBT* p1 = new NodeRBT(13, 0);
-//    NodeRBT* p2 = new NodeRBT(10, 0);
-//    if (*p1 < *p2)
-//        p1 = nullptr;
     return 0;
 }
